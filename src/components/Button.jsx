ddacdb244d396723073903d4c697269372bc30e3
Arturs <arrturs.torsters@gmail.com>
2023-05-07 14:26:56 +0300

    init

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Button = () => {
+  return <button className="mx-auto d-block bg-primary text-white py-2 px-2">Click me</button>;
+};
+
+export default Button;
---
+++ src/pages/App.js
@@ -0,0 +1,26 @@
+import Button from "../components/Button";
+import Navbar from "../components/Navbar";
+
+
+function App() {
+  return (
+    <div className="App ">
+      <header className="App-header">
+      <Navbar />
+
+      </header>
+
+      <body>
+
+
+      </body>
+
+      <footer>
+
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
